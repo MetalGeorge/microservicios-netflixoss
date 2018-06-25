9ece6bb5c828f97efeb8c4807e888333de6c3d85
MetalGeorge <jorgeescobar777@gmail.com>
2018-06-25 00:28:38 -0400

    changed import org.apache.commons.lang.builder.ToStringBuilder

--- microservice-demo-catalog/src/main/java/com/ewolff/microservice/catalog/Item.java
+++ microservice-demo-catalog/src/main/java/com/ewolff/microservice/catalog/Item.java
@@ -7,7 +7,7 @@ import javax.persistence.Id;
 
 import org.apache.commons.lang.builder.EqualsBuilder;
 import org.apache.commons.lang.builder.HashCodeBuilder;
-import org.apache.commons.lang.builder.ToStringBuilder
+import org.apache
 
 @Entity
 public class Item {
